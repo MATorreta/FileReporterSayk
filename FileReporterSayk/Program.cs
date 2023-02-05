281f4f22d0d5117df80b48c926707b43ccbabd8a
MATorreta <migueltorrecillamora@gmail.com>
2023-02-05 06:14:05 -0800

    Add project files.

---
+++ FileReporterSayk/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Net.NetworkInformation;
+
+namespace FileReporterSayk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List of Drives");
+            DriveInfo[] drives = DriveInfo.GetDrives();                                                        //use library sistem.IO  for use "get Drives",save in array all drives.
+            for (int i = 0; i < drives.Length; i++)                                                             //iterate array  
+            {
+                Console.WriteLine($"{i + 1}.{drives[i].Name} - {Utilities.BytesToSize(drives[i].TotalSize)}");   // Utilities.BytesToSize:call function BytesToSize created in Utilities.
+            }
+            int selected;
+            bool error = false;
+            do
+            {
+                Console.WriteLine("Choose Drive for analyze");
+                error = int.TryParse(Console.ReadLine(), out selected);
+                if (selected > drives.Length || selected <= 0 || !error)
+                {
+                    Console.WriteLine("Invalid entry. Try again.");
+                }
+            } while (selected > drives.Length || selected <= 0 || !error);
+
+            DriveInfo selectedDrive = drives[selected - 1];                                                  // -1 because Array start in 0, but the list start in 1.        
+            Console.WriteLine("Drive selected: " + selectedDrive);
+            Utilities.GetFileInfo(selectedDrive.Name);                                                       //Call function in Utilities.GetFileInfo for selected drive.
+            Utilities.GetSubFoldersInfo(selectedDrive.Name);
+
+        }
+    }
+}
---
+++ FileReporterSayk/Utilities.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReporterSayk
+{
+    internal class Utilities
+    {
+
+        public static Dictionary<string, PathStats> Statistics = new Dictionary<string, PathStats>();
+
+        public static void GetFileInfo(string path)
+        {
+            long currentPathSize = 0;
+            int currentPathItems = 0;
+
+            DirectoryInfo di = new DirectoryInfo(path);
+            try
+            {
+                foreach (FileInfo file in di.GetFiles()) 
+                {
+                    currentPathSize += file.Length;
+                    currentPathItems++;    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\t" + path);
+            }
+            PathStats currentPathStats = new PathStats(currentPathSize, currentPathItems);
+            Statistics.Add(path, currentPathStats);
+            Console.WriteLine(path + "\t" + Utilities.BytesToSize(currentPathStats.TotalSize) +"\t" +currentPathStats.NumberItems + " Items ");
+
+        }
+        public static void GetSubFoldersInfo(string path)
+        {
+            DirectoryInfo selectedDi = new DirectoryInfo(path);
+            DirectoryInfo[] folders = selectedDi.GetDirectories();
+
+
+            foreach (DirectoryInfo folder in folders)
+            {
+                try
+                {
+                    Utilities.GetFileInfo(folder.FullName);
+                    DirectoryInfo currentFolder = new DirectoryInfo(folder.FullName);
+                    GetSubFoldersInfo(folder.FullName);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message + "\t" + folders);
+                }
+
+
+            }
+        }
+        public static string BytesToSize(long bytes)                  //Function for convert Bytes on GB, MB , KB. 
+        {
+            if (bytes >= 1073741824)
+            {
+                return (bytes / 1073741824).ToString() + " GB ";
+            }
+            else if (bytes >= 1048576)
+            {
+                return (bytes / 1048576).ToString() + " MG ";
+            }
+            else if (bytes >= 1024)
+            {
+                return (bytes / 1024).ToString() + " KB ";
+            }
+            else
+                return bytes + "Bytes";       
+        }
+    }    
+}
